098d7e16d63fac5c38c0ae25c63051ecc0bbdde6
Jeffrey Ross-Ibarra <rossibarra@gmail.com>
2015-01-13 14:54:52 -0800

    Jeff revisits of mutation

--- rcode/Area_Script.R
+++ rcode/Area_Script.R
@@ -4,13 +4,13 @@ require(raster)
 require(ncdf)
 
 #load data, plot area of interest in green
-Altitude=raster("alt.bil") #altitude data from worldclim
-Maize=raster("maize_AreaYieldProduction.nc",level=1) #maize cultivation data from earthstat; first level is percent land cultivated
+Altitude=raster("~/Desktop/area/alt.bil") #altitude data from worldclim
+Maize=raster("~/Desktop/area/maize_AreaYieldProduction.nc",level=1) #maize cultivation data from earthstat; first level is percent land cultivated
 e<-extent(c(-89.82036,-28.74251,-56.5066,12.90124)) #defines extent of South America
 SAAlt<-crop(Altitude,e) #apply extent to altitude layer
 SAMaize<-crop(Maize,e) #apply extent to maize layer
 a<-SAAlt>1700 #define threshold for altitude
-m<-SAMaize>0 #define threshold for proportion of pixel under maize cultivation
+m<-SAMaize>0.01 #define threshold for proportion of pixel under maize cultivation
 intersect<-a+m #add layers with raster algebra; layers meeting both criteria (altitude and maize cultivation have value of "2")
 plot(intersect, main="Intersect of Maize Cultivation > 0 and Altitude >1700m")
 
@@ -24,5 +24,5 @@ projection(SAMaize)<-"+proj=utm +zone=48 +datum=WGS84" #to stack layer each must
 ST<-stack(SAAlt,SAMaize) #stacking altitude and maize layers so attributes can be retained in a query
 STM<-as.matrix(ST) #converting S4 object to matrix
 STD<-data.frame(STM) #converting matrix to data.frame for subset query
-sub<-subset(STD, alt > 1700 & maizeData > 0) # identifying subset of grids with maize cultivation and >1700m
+sub<-subset(STD, alt > 1700 & maizeData > 0.05) # identifying subset of grids with maize cultivation and >1700m
 sum(sub$maizeData)*100 #calculating area
